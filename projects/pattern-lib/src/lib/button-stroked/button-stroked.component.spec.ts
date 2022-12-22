957289777b4fa53ab8959254316d39e73020ce41
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2022-12-23 02:21:26 +0700

    feat: add flat and stroked button

---
+++ projects/pattern-lib/src/lib/button-stroked/button-stroked.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ButtonStrokedComponent } from './button-stroked.component';
+
+describe('ButtonStrokedComponent', () => {
+  let component: ButtonStrokedComponent;
+  let fixture: ComponentFixture<ButtonStrokedComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ButtonStrokedComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ButtonStrokedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- projects/pattern-lib/src/lib/pattern-lib.module.ts
+++ projects/pattern-lib/src/lib/pattern-lib.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PatternLibComponent } from './pattern-lib.component';
-import { ButtonSemiRoundedComponent } from './button-semi-rounded/button-semi-rounded.component';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 import { CommonModule } from '@angular/common';
+import { ButtonFlatComponent } from './button-flat/button-flat.component';
+import { ButtonStrokedComponent } from './button-stroked/button-stroked.component';
 
 @NgModule({
-  declarations: [PatternLibComponent, ButtonSemiRoundedComponent],
-  imports: [MatButtonModule, CommonModule],
-  exports: [PatternLibComponent, ButtonSemiRoundedComponent],
+  declarations: [
+    PatternLibComponent,
+    ButtonFlatComponent,
+    ButtonStrokedComponent,
+  ],
+  imports: [MatButtonModule, CommonModule, MatIconModule],
+  exports: [PatternLibComponent, ButtonFlatComponent],
 })
 export class PatternLibModule {}
